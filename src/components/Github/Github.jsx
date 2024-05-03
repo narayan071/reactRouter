5a281583748032302c99fa7ad971c49bfe134b80
narayan071 <narayan.pradhan1117@gmail.com>
2024-05-03 20:07:46 +0530

    finish

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
-
+import { useLoaderData } from 'react-router-dom'
 function Github() {
+    const data = useLoaderData();
   return (
-    <div className='text-center bg-gray-700'>
-        
+    <div className='text-center bg-gray-700 flex'>
+        <div className="w-1/2 text-center text-white my-auto">
+            <h1>Github UserName: {data.login}</h1>
+            <h1>Name: {data.name}</h1>
+            <h1>Followers : {data.followers}</h1>
+        </div>
+        <div className="w-1/2">
+            <img src={data.avatar_url} className='rounded-full shadow-2xl p-3' alt="avatar" />
+        </div>
     </div>
   )
 }
 
 export default Github
+
+export const dataLoader = async() =>{
+    const response = await fetch("https://api.github.com/users/narayan071")
+    return response.json();
+}
--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,14 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
-import Github from './components/Github/Github.jsx'
+import Github, {dataLoader} from './components/Github/Github.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout/>}>
       <Route path="" element={<Home/>}/>
       <Route path="about/" element={<About/>}/>
       <Route path="contact/" element={<Contact/>} />
-      <Route path="github/" element={<Github/>} />
+      <Route loader={dataLoader} path="github/" element={<Github/>} />
     </Route>
   )
 )
